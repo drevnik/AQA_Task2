2ba71a456ec97d847c0bce4c63d488bad7975d4d
Natalya Drevnik <natalyadrevnik@mail.ru>
2020-01-24 19:27:43 +0300

    Объединение тест-кейсов в тест-сьют

---
+++ src/test/java/ru/netology/CashbackHackServiceTests.java
@@ -0,0 +1,36 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CashbackHackServiceTests {
+    @Test
+    void shouldReturn0IfAmountIs1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturn100IfAmountIs900() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturn999IfAmountIs1() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1001;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 999;
+        assertEquals(expected, actual);
+    }
+
+
+}
